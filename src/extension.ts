d7485e4ce3f351999f793ffd5907d320caf4f3b1
Tim TJey Jun <tjunussov@gmail.com>
2020-08-05 03:01:09 +0600

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,26 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import axios from 'axios';
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('catCoding.start', () => {
+      // Create and show panel
+      const panel = vscode.window.createWebviewPanel(
+        'catCoding',
+        'Cat Coding',
+        vscode.ViewColumn.One,
+        {}
+      );
+
+      // And set its HTML content
+      
+    })
+  );
+}
+
+
+
 
 export function activate(context: vscode.ExtensionContext) {
   // TODO: collect all errors and send feedback
@@ -18,29 +37,12 @@ export function activate(context: vscode.ExtensionContext) {
     }).then(url => {
       if (url) {
         const panel = vscode.window.createWebviewPanel(
-          'browserTab',
-          'browserTab',
-          vscode.ViewColumn.Active, {
-            enableScripts: true
-          }
+          'Browser LiveView',
+          'Browser LiveView',
+          vscode.ViewColumn.One,
+          {}
         );
-        // http:// || https://
-        const host = url.substring(0, url.indexOf('//' + 2));
-        // Get HTML content of site
-        getWebContent(url).then((content) => {
-          // Convert link to style
-          getStyles(host, url, content.data).then((styles) => {
-            // Show data
-            panel.webview.html = [
-              styles,
-              content.data
-            ].join(' ');
-          }).catch((e) => {
-            console.error('getStyles error', e);
-          });
-        }).catch((e) => {
-          console.error('getWebContent error', e);
-        });
+        panel.webview.html = getWebviewContent(url);
       } else {
         vscode.window.showInformationMessage('The url can be specified! Do nothing');
       }
@@ -54,67 +56,18 @@ export function deactivate() {
 }
 
 /**
- * TODO: collect all errors and send feedback
- * Convert <link> to <style>
- * @param {string} host Host of site (http://|https://)
- * @param {string} url Base url
- * @param {string} content HTML content by url
- */
-function getStyles(host: string, url: string, content: string): Promise<string> {
-  return new Promise<string>((resolve) => {
-    // Regexp for find all html tag <link>
-    const findLinkRegexp = /\<link\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/g,
-      // Regexp for test only stylesheet <link>
-      findStyleSheetRegexp = /rel=\"stylesheet\"/,
-      // Regexp for find attribute href in <link>
-      findLinkHrefRegexp = /href=\"(.*?)\"/,
-      // All founded <link>
-      links = content.match(findLinkRegexp);
-    if (links && Array.isArray(links)) {
-      // Css inline styles
-      const cssStyles: string[] = [],
-        // All axios promises for getting raw css
-        cssPromises: Promise<any>[] = [];
-      links.forEach((item) => {
-        // If this link is stylesheet
-        if (findStyleSheetRegexp.test(item)) {
-          // Try find href attribute
-          const needle = item.match(findLinkHrefRegexp);
-          // If found
-          if (needle && Array.isArray(needle) && needle.hasOwnProperty('1')) {
-            // Build url for getting css
-            const cssUrl = (needle[1].substring(0, 2) === '//')
-              ? host + needle[1].substring(2, needle[1].length)
-              : url + '/' + needle[1];
-            cssPromises.push(axios.get(cssUrl));
-          }
-        }
-      });
-      if (cssPromises) {
-        // Getting all css's
-        Promise.all(cssPromises).then((values) => {
-          values.forEach((item) => {
-            // Genetare inline style
-            cssStyles.push([
-              '<style type="text/css">',
-              item.data,
-              '</style>'
-            ].join(' '));
-          });
-          resolve(cssStyles.join(' '));
-        }).catch((e) => {
-          console.error('cssPromises error', e);
-        });
-      }
-    } else {
-      resolve('');
-    }
-  });
-}
-
-/**
  * Get web content by URL
  */
-function getWebContent(url: string): Promise<any> {
-  return axios.get(url);
+function getWebviewContent(url: string) {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>URL</title>
+</head>
+<body>
+    <iframe src="${url}" style="width: 100%; height: 100%; min-height: 0px; border-width: 0px; overflow: auto;"/>
+</body>
+</html>`;
 }
